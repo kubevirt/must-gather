3c90147c86015ce81906e382cbbae2e88407e960
Simone Tiraboschi <stirabos@redhat.com>
2021-10-20 12:26:20 +0200

    test: gather more resources
    Create and collect 100 VMs (starting only 5)
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- automation/create_workloads.sh
+++ automation/create_workloads.sh
@@ -4,13 +4,23 @@ set -ex
 
 CMD=oc
 
-for ns in {1..5}; do
-  sed "s#__NS__#${ns}#g" automation/vm.yaml | ${CMD} apply -f -
+# create 100 VMs
+for ns in {001..005}; do
+  sed "s#__NS__#${ns}#g" automation/ns.yaml | ${CMD} apply -f -
+  for vm in {001..020}; do
+    sed -e "s#__NS__#${ns}#g" -e "s|##VM##|${vm}|g" automation/vm.yaml >> "vms_ns${ns}.yaml"
+  done
+  ${CMD} apply -f "vms_ns${ns}.yaml"
+
+  # start 5 VMs
+  if [[ ${ns} -le 5 ]]; then
+    ${CMD} patch -n "ns${ns}" virtualmachine "testvm-ns${ns}-vm001" --type merge -p '{"spec":{"running":true}}'
+  fi
 done
 
-
-for ns in {1..5}; do
-  ${CMD} wait -n "ns${ns}" vmi "testvm${ns}" --for condition=Ready --timeout="300s"
+# wait for the execution of the first 5 VMs
+for ns in {001..005}; do
+  ${CMD} wait -n "ns${ns}" vmi "testvm-ns${ns}-vm001" --for condition=Ready --timeout="300s"
 done
 
 ${CMD} get vmis --all-namespaces
