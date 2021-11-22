5d97b8dddadd07cbb7e3ab668b35974199234324
Nahshon Unna Tsameret <60659093+nunnatsa@users.noreply.github.com>
2021-11-22 10:38:59 +0200

    Gather VM CR by default (#106)
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- automation/create_workloads.sh
+++ automation/create_workloads.sh
@@ -4,11 +4,19 @@ set -ex
 
 CMD=oc
 
+ANNOTATION_EXP="/^  namespace: ns.*$/a\  annotations:\n    vm.kubevirt.io/template: some-template"
+
 # create 100 VMs
 for ns in {001..005}; do
   sed "s#__NS__#${ns}#g" automation/ns.yaml | ${CMD} apply -f -
   for vm in {001..020}; do
-    sed -e "s#__NS__#${ns}#g" -e "s|##VM##|${vm}|g" automation/vm.yaml >> "vms_ns${ns}.yaml"
+    n=$(echo $vm | sed "s|^0*||")
+    if [[ $(($n%2)) -eq 0 ]]; then
+      sed -e "s#__NS__#${ns}#g" -e "s|##VM##|${vm}|g" -e "${ANNOTATION_EXP}" automation/vm.yaml >> "vms_ns${ns}.yaml"
+    else
+      sed -e "s#__NS__#${ns}#g" -e "s|##VM##|${vm}|g" automation/vm.yaml >> "vms_ns${ns}.yaml"
+    fi
+
   done
   ${CMD} apply -f "vms_ns${ns}.yaml"
 
