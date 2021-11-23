eed2730334d6ef9eaba56274dad44e39391b2d41
Nahshon Unna Tsameret <60659093+nunnatsa@users.noreply.github.com>
2021-11-23 20:56:49 +0200

    [Release 4.9] Adopt the bug fixes and logs from the main branch (#108)
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

---
+++ collection-scripts/logs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+export BASE_COLLECTION_PATH="${BASE_COLLECTION_PATH:-/must-gather}"
+NAMESPACE_FILE=/var/run/secrets/kubernetes.io/serviceaccount/namespace
+
+# if running in a pod
+if [[ -f ${NAMESPACE_FILE} ]]; then
+  POD=$(oc status | grep "^pod" | sed -E "s|pod/([^ ]+).*|\1|")
+  oc logs --timestamps=true -n $(cat ${NAMESPACE_FILE}) ${POD} -c gather > ${BASE_COLLECTION_PATH}/must-gather.log
+fi
