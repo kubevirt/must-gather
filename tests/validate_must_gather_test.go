731959915dd231e387f6bac72a8538f0b8af105a
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2021-11-23 15:20:06 +0200

    [Release 4.9] Adopt the bug fixes and logs from the main branch
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- tests/validate_must_gather_test.go
+++ tests/validate_must_gather_test.go
@@ -20,12 +20,14 @@ var logger = log.New(GinkgoWriter, "", 0)
 
 var _ = Describe("validate the must-gather output", func() {
 	logger := log.New(GinkgoWriter, "", 0)
+
 	outputDir, err := getDataDir()
-	It("should find the directory", func() {
+
+	It("[level:product][level:workloads]should find the directory", func() {
 		Expect(err).ToNot(HaveOccurred())
 	})
 
-	Context("validate the installation namespace", func() {
+	Context("[level:product]validate the installation namespace", func() {
 		It("should validate the kubevirt-hyperconverged/crs directory", func() {
 
 			var installationNamespace = "kubevirt-hyperconverged"
@@ -93,7 +95,7 @@ var _ = Describe("validate the must-gather output", func() {
 		logger.Print("outputDir:", outputDir)
 	})
 
-	Context("validate the cluster scoped resources", func() {
+	Context("[level:product]validate the cluster scoped resources", func() {
 		It("should validate the cluster-scoped-resources directory", func() {
 
 			crsDir := path.Join(outputDir, "cluster-scoped-resources")
@@ -160,7 +162,7 @@ var _ = Describe("validate the must-gather output", func() {
 		logger.Print("outputDir:", outputDir)
 	})
 
-	Context("validate nodes logs", func() {
+	Context("[level:product]validate nodes logs", func() {
 		It("should validate the nodes logs directories", func() {
 
 			expectedResources := []string{
@@ -202,79 +204,121 @@ var _ = Describe("validate the must-gather output", func() {
 		logger.Print("outputDir:", outputDir)
 	})
 
-	Context("validate workloads", func() {
-		DescribeTable("validate workloads", func(ns string) {
-			expectedResources := map[string]bool{
-				"datavolumes.cdi.kubevirt.io":         false,
-				"virtualmachineinstances.kubevirt.io": false,
-				"virtualmachines.kubevirt.io":         false,
-			}
+	Context("[level:workloads]validate workloads", func() {
+		DescribeTable("validate workloads", func(namespace string) {
 
-			namespace := "ns" + ns
-
-			crsDir := path.Join(outputDir, "namespaces", namespace, "crs")
-			crs, err := os.ReadDir(crsDir)
+			vmFile, err := os.Open(path.Join(outputDir, "namespaces", namespace, "kubevirt.io", "virtualmachines.yaml"))
 			Expect(err).ToNot(HaveOccurred())
+			defer vmFile.Close()
 
-			for expectedResource := range expectedResources {
-				Expect(fileInDir(crs, expectedResource))
+			vms, err := getObjectFromFile(vmFile)
+			Expect(vms.Object["items"]).To(HaveLen(20))
+
+			for i, vm := range vms.Object["items"].([]interface{}) {
+				expectedName := fmt.Sprintf("testvm-%s-vm%03d", namespace, i+1)
+				md := vm.(map[string]interface{})["metadata"].(map[string]interface{})
+				Expect(md["name"]).To(Equal(expectedName))
+
+				objFromCluster, err := client.getNamespacedResource(context.Background(), "virtualmachines", "kubevirt.io", namespace, expectedName)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(reflect.DeepEqual(vm.(map[string]interface{})["spec"], objFromCluster.Object["spec"])).Should(BeTrue())
 			}
 
-			for _, cr := range crs {
-				if _, found := expectedResources[cr.Name()]; !found {
-					continue
-				}
+			vmiFile, err := os.Open(path.Join(outputDir, "namespaces", namespace, "kubevirt.io", "virtualmachineinstances.yaml"))
+			Expect(err).ToNot(HaveOccurred())
+			defer vmiFile.Close()
 
-				Expect(cr.IsDir()).To(BeTrue(), cr.Name(), " should be a directory")
-				crDir := path.Join(crsDir, cr.Name())
-				crFiles, err := os.ReadDir(crDir)
-				Expect(err).ToNot(HaveOccurred())
-				Expect(crFiles).Should(HaveLen(1))
+			vmis, err := getObjectFromFile(vmiFile)
+			Expect(vmis.Object["items"]).To(HaveLen(1))
+			vmi := vmis.Object["items"].([]interface{})[0].(map[string]interface{})
+			expectedName := fmt.Sprintf("testvm-%s-vm001", namespace)
+			Expect(vmi["metadata"].(map[string]interface{})["name"]).To(Equal(expectedName))
+			objFromCluster, err := client.getNamespacedResource(context.Background(), "virtualmachineinstances", "kubevirt.io", namespace, expectedName)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(reflect.DeepEqual(vmi["spec"], objFromCluster.Object["spec"])).Should(BeTrue())
 
-				if crFiles[0].IsDir() {
-					continue
-				}
+			vmDir := path.Join(outputDir, "namespaces", namespace, "vms", expectedName)
+			dir, err := os.ReadDir(vmDir)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(dir).To(HaveLen(5))
 
-				file, err := os.Open(path.Join(crDir, crFiles[0].Name()))
-				Expect(err).ToNot(HaveOccurred())
-
-				ext := path.Ext(file.Name())
-				Expect(ext).Should(Equal(".yaml"), fmt.Sprintf("file %s is not a yaml file", file.Name()))
-				resourceName := path.Base(file.Name())
-				resourceName = resourceName[:len(resourceName)-len(ext)]
-
-				Expect(resourceName).To(ContainSubstring(ns))
-
-				resourceTypeSplit := strings.Split(cr.Name(), ".")
-				resourceType := resourceTypeSplit[0]
-				resourceGroup := strings.Join(resourceTypeSplit[1:], ".")
-
-				objFromCluster, err := client.getNamespacedResource(context.Background(), resourceType, resourceGroup, namespace, resourceName)
-				Expect(err).ToNot(HaveOccurred())
-
-				objFromFile, err := getObjectFromFile(file)
-				Expect(err).ToNot(HaveOccurred())
-
-				clusterSpec, found := objFromCluster.Object["spec"]
-				Expect(found).To(BeTrue())
-				fileSpec, found := objFromFile.Object["spec"]
-				Expect(found).To(BeTrue())
-
-				Expect(reflect.DeepEqual(fileSpec, clusterSpec)).Should(BeTrue())
-
-				expectedResources[cr.Name()] = true
+			fileExists := map[string]bool{
+				"bridge.txt":   false,
+				"dumpxml.xml":  false,
+				"iptables.txt": false,
+				"ip.txt":       false,
+				"qemu.log":     false,
 			}
-			Expect(expectedResources).To(BeAllTrueInBoolMap())
 
+			dotLoc := strings.Index(dir[0].Name(), ".")
+			podName := dir[0].Name()[:dotLoc]
+
+			for _, f := range dir {
+				fileExists[f.Name()[dotLoc+1:]] = true
+			}
+
+			Expect(fileExists).To(BeAllTrueInBoolMap())
+
+			podFile, err := os.Open(path.Join(outputDir, "namespaces", namespace, "pods", podName, podName+".yaml"))
+			Expect(err).ToNot(HaveOccurred())
+			podObj, err := getPodFromFile(podFile)
+			Expect(err).ToNot(HaveOccurred())
+			pod, err := client.getPod(context.Background(), namespace, podName)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(reflect.DeepEqual(podObj.Spec, pod.Spec)).Should(BeTrue())
 		},
-			Entry("should gather resources in ns1", "1"),
-			Entry("should gather resources in ns2", "2"),
-			Entry("should gather resources in ns3", "3"),
-			Entry("should gather resources in ns4", "4"),
-			Entry("should gather resources in ns5", "5"),
+			Entry("should gather resources in ns001", "ns001"),
+			Entry("should gather resources in ns002", "ns002"),
+			Entry("should gather resources in ns003", "ns003"),
+			Entry("should gather resources in ns004", "ns004"),
+			Entry("should gather resources in ns005", "ns005"),
+		)
+	})
+
+	Context("[level:product]validate workloads", func() {
+		// This test assumes, according to automation/create_workloads.sh, that "odd vms", like testvm-ns001-vm003, are
+		// custom VMs, and "even vms", like testvm-ns003-vm008, are template based vms.
+		DescribeTable("validate virtual machines", func(namespace string) {
+			vmsDir := path.Join(outputDir, "namespaces", namespace, "kubevirt.io", "virtualmachines")
+
+			for i := 1; i <= 20; i++ {
+				vmName := fmt.Sprintf("testvm-%s-vm%03d", namespace, i)
+				vmType := "template-based"
+				if i%2 == 1 {
+					vmType = "custom"
+				}
+				vmPath := path.Join(vmsDir, vmType, vmName+".yaml")
+				validateVmFile(vmName, namespace, vmPath)
+
+			}
+		},
+			Entry("should gather resources in ns001", "ns001"),
+			Entry("should gather resources in ns002", "ns002"),
+			Entry("should gather resources in ns003", "ns003"),
+			Entry("should gather resources in ns004", "ns004"),
+			Entry("should gather resources in ns005", "ns005"),
 		)
 	})
 })
+
+func validateVmFile(vm, ns, vmPath string) {
+	file, err := os.Open(vmPath)
+	ExpectWithOffset(1, err).ToNot(HaveOccurred(), "can't open the %s file", vmPath)
+
+	objFromCluster, err := client.getNamespacedResource(context.Background(), "virtualmachines", "kubevirt.io", ns, vm)
+	Expect(err).ToNot(HaveOccurred())
+
+	objFromFile, err := getObjectFromFile(file)
+	Expect(err).ToNot(HaveOccurred())
+
+	clusterSpec, found := objFromCluster.Object["spec"]
+	Expect(found).To(BeTrue())
+	fileSpec, found := objFromFile.Object["spec"]
+	Expect(found).To(BeTrue())
+
+	Expect(reflect.DeepEqual(fileSpec, clusterSpec)).Should(BeTrue())
+
+}
 
 func getDataDir() (string, error) {
 	wd, err := os.Getwd()
