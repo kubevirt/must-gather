5c35ef6d5a986211c556b0a295a585fdb015d10a
Simone Tiraboschi <stirabos@redhat.com>
2024-05-27 17:37:06 +0200

    Fix gather_nodes execution with multiple images
    POD_NAME is already available out of the box as
    an env variable via downwardAPIs, no need to introspect
    it from the list of running pods.
    POD_NAME introspection is currently broken when
    multiple images are executed in parallel.
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- collection-scripts/logs.sh
+++ collection-scripts/logs.sh
@@ -8,6 +8,5 @@ NAMESPACE_FILE=/var/run/secrets/kubernetes.io/serviceaccount/namespace
 
 # if running in a pod
 if [[ -f ${NAMESPACE_FILE} ]]; then
-  POD=$(oc status | grep "^pod" | sed -E "s|pod/([^ ]+).*|\1|")
-  oc logs --timestamps=true -n "$(cat ${NAMESPACE_FILE})" "${POD}" -c gather > "${BASE_COLLECTION_PATH}/must-gather.log"
+  oc logs --timestamps=true -n "$(cat ${NAMESPACE_FILE})" "${POD_NAME}" -c gather > "${BASE_COLLECTION_PATH}/must-gather.log"
 fi
